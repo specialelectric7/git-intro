e29f520d37dc23841707abf6a55d2ba267adfe4f
specialelectric7 <specialelectric7@users.noreply.github.com>
2019-08-21 07:59:34 -0400

    Create ip-addrs.py

---
+++ ip-addrs.py
@@ -0,0 +1,14 @@
+
+# create list of ip addresses
+
+ip_addr_list = ['10.1.1.1','10.1.1.2','10.1.1.3','10.1.1.4','10.1.1.5']
+
+# add some more ips we forgot about...
+
+ip_addr_list.append('10.1.1.6')
+ip_addr_list.append('10.1.1.7')
+
+# print out our list
+
+for ip_addr in ip_addr_list:
+    print(ip_addr)
